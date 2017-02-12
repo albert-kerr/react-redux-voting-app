6da2eb1695d5cf92acecea565c3af53aa5657079
Albert Kerr <ankerr305@gmail.com>
2017-02-12 18:04:48 +1100

    Starting project from scratch

--- src/core.js
+++ src/core.js
@@ -3,72 +3,43 @@ import {List, Map} from 'immutable';
 export const INITIAL_STATE = Map();
 
 export function setEntries(state, entries) {
-  const list = List(entries);
-  return state.set('entries', list)
-              .set('initialEntries', list);
+	return state.set('entries', List(entries));
 }
 
 function getWinners(vote) {
   if (!vote) return [];
-  const [one, two] = vote.get('pair');
-  const oneVotes = vote.getIn(['tally', one], 0);
-  const twoVotes = vote.getIn(['tally', two], 0);
-  if      (oneVotes > twoVotes)  return [one];
-  else if (oneVotes < twoVotes)  return [two];
-  else                           return [one, two];
+
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const aVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes) return [a];
+  else if (aVotes < bVotes) return [b];
+  else                      return [a, b];
 }
 
-export function next(state, round = state.getIn(['vote', 'round'], 0)) {
-  const entries = state.get('entries')
+export function next(state) {
+	const entries = state.get('entries')
                        .concat(getWinners(state.get('vote')));
-  if (entries.size === 1) {
+
+  if (entries.size == 1)
     return state.remove('vote')
                 .remove('entries')
-                .set('winner', entries.first());
-  } else {
-    return state.merge({
-      vote: Map({
-        round: round + 1,
-        pair: entries.take(2)
-      }),
-      entries: entries.skip(2)
-    });
-  }
+                .set('winners', entries.first());
+
+	return state.merge({
+		vote: Map({pair: entries. take(2)}),
+		entries: entries.skip(2)
+	});
 }
 
-export function restart(state) {
-  const round = state.getIn(['vote', 'round'], 0);
-  return next(
-    state.set('entries', state.get('initialEntries'))
-         .remove('vote')
-         .remove('winner'),
-    round
-  );
+export function vote(state, entry) {
+	return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+	);
 }
 
-function removePreviousVote(voteState, voter) {
-  const previousVote = voteState.getIn(['votes', voter]);
-  if (previousVote) {
-    return voteState.updateIn(['tally', previousVote], t => t - 1)
-                    .removeIn(['votes', voter]);
-  } else {
-    return voteState;
-  }
-}
 
-function addVote(voteState, entry, voter) {
-  if (voteState.get('pair').includes(entry)) {
-    return voteState.updateIn(['tally', entry], 0, t => t + 1)
-                    .setIn(['votes', voter], entry);
-  } else {
-    return voteState;
-  }
-}
 
-export function vote(voteState, entry, voter) {
-  return addVote(
-    removePreviousVote(voteState, voter),
-    entry,
-    voter
-  );
-}
+
--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -11,13 +11,12 @@ describe('reducer', () => {
     const nextState = reducer(initialState, action);
 
     expect(nextState).to.equal(fromJS({
-      entries: ['Trainspotting'],
-      initialEntries: ['Trainspotting']
+      entries: ['Trainspotting']
     }));
   });
 
   it('handles NEXT', () => {
-    const initialState = fromJS({
+		const initialState = fromJS({
       entries: ['Trainspotting', '28 Days Later']
     });
     const action = {type: 'NEXT'};
@@ -25,7 +24,6 @@ describe('reducer', () => {
 
     expect(nextState).to.equal(fromJS({
       vote: {
-        round: 1,
         pair: ['Trainspotting', '28 Days Later']
       },
       entries: []
@@ -35,50 +33,19 @@ describe('reducer', () => {
   it('handles VOTE', () => {
     const initialState = fromJS({
       vote: {
-        round: 1,
         pair: ['Trainspotting', '28 Days Later']
       },
       entries: []
     });
-    const action = {type: 'VOTE', entry: 'Trainspotting', clientId: 'voter1'};
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
     const nextState = reducer(initialState, action);
 
     expect(nextState).to.equal(fromJS({
       vote: {
-        round: 1,
         pair: ['Trainspotting', '28 Days Later'],
-        tally: {Trainspotting: 1},
-        votes: {
-          voter1: 'Trainspotting'
-        }
+        tally: {Trainspotting: 1}
       },
       entries: []
-    }));
-  });
-
-  it('has an initial state', () => {
-    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
-    const nextState = reducer(undefined, action);
-    expect(nextState).to.equal(fromJS({
-      entries: ['Trainspotting'],
-      initialEntries: ['Trainspotting']
-    }));
-  });
-
-  it('can be used with reduce', () => {
-    const actions = [
-      {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
-      {type: 'NEXT'},
-      {type: 'VOTE', entry: 'Trainspotting', clientId: 'voter1'},
-      {type: 'VOTE', entry: '28 Days Later', clientId: 'voter2'},
-      {type: 'VOTE', entry: 'Trainspotting', clientId: 'voter3'},
-      {type: 'NEXT'}
-    ];
-    const finalState = actions.reduce(reducer, Map());
-
-    expect(finalState).to.equal(fromJS({
-      winner: 'Trainspotting',
-      initialEntries: ['Trainspotting', '28 Days Later']
     }));
   });
 
