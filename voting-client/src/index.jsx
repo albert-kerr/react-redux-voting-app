dffaa26a0dd823990d8f6697b81bb82675dc68b8
Albert Kerr <ankerr305@gmail.com>
2017-02-14 22:40:22 +1100

    Added result component + unit test

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Voting from'./components/Voting';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
+import Voting from './components/Voting';
+import Results from './components/Results'
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
-	<Voting pair={pair} winner="Trainspotting"/>,
-	document.getElementById('app')
+  <Router history={hashHistory}>{routes}</Router>,
+  document.getElementById('app')
 );
