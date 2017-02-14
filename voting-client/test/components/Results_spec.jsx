5cd3381fa44e281b6ecc433cefea6804afddf2ab
Albert Kerr <ankerr305@gmail.com>
2017-02-14 23:05:54 +1100

    Finished UI components

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithClass
+  scryRenderedDOMComponentsWithClass,
+  Simulate
 } from 'react-addons-test-utils';
 import {List, Map} from 'immutable';
 import Results from '../../src/components/Results';
 import {expect} from 'chai';
+
 
 describe('Results', () => {
 
@@ -25,4 +28,31 @@ describe('Results', () => {
     expect(days).to.contain('0');
   });
 
+  it('invokes the next callback when next button is clicked', () => {
+    let nextInvoked = false;
+    const next = () => nextInvoked = true;
+
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const component = renderIntoDocument(
+      <Results pair={pair}
+               tally={Map()}
+               next={next}/>
+    );
+    Simulate.click(ReactDOM.findDOMNode(component.refs.next));
+
+    expect(nextInvoked).to.equal(true);
+  });
+
+  it('renders the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Results winner="Trainspotting"
+               pair={["Trainspotting", "28 Days Later"]}
+               tally={Map()} />
+    );
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
+  });
+
+  
 });
